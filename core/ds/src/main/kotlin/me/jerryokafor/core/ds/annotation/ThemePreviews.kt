952d423c49a649f32fdc707f5362161e3ef64a2e
Jerry Okafor <jerryhanksokafor@gmail.com>
2023-08-24 22:14:42 -0600

    feat : GH-27 Add movies screen, and test        GH-21 Move all android test to test only module        Add search and movie details scaffold.        Fix : add class to jacoco exclude annotation

--- core/ds/src/main/kotlin/me/jerryokafor/core/ds/annotation/ThemePreviews.kt
+++ core/ds/src/main/kotlin/me/jerryokafor/core/ds/annotation/ThemePreviews.kt
@@ -26,6 +26,7 @@ package me.jerryokafor.core.ds.annotation
 
 import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
+import me.jerryokafor.core.common.annotation.ExcludeFromJacocoGeneratedReport
 
 /**
  * Multi-preview annotation that represents light and dark themes. Add this annotation to a
@@ -33,4 +34,5 @@ import androidx.compose.ui.tooling.preview.Preview
  */
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark theme")
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light theme")
+@ExcludeFromJacocoGeneratedReport
 annotation class ThemePreviews
