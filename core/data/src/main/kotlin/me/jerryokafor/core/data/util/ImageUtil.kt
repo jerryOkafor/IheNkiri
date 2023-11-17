ffde5a980477023111dbeb4f3a5be6a1eec83cae
Jerry Okafor <jerryhanksokafor@gmail.com>
2023-11-16 17:47:43 -0700

    chore : GH-40 Increase test coverage.         Add MovieDetailsApiTest

--- core/data/src/main/kotlin/me/jerryokafor/core/data/util/ImageUtil.kt
+++ core/data/src/main/kotlin/me/jerryokafor/core/data/util/ImageUtil.kt
@@ -31,7 +31,7 @@ object ImageUtil {
         enum class BackDrop(override val value: String) : Size {
             W300("w300"),
             W780("w780"),
-            W1280("w1280"),
+            W1280("w1280")
         }
 
         enum class Logo(override val value: String) : Size {
@@ -40,7 +40,7 @@ object ImageUtil {
             W154("w154"),
             W185("w185"),
             W300("w300"),
-            W500("w500"),
+            W500("w500")
         }
 
         enum class Poster(override val value: String) : Size {
@@ -49,19 +49,19 @@ object ImageUtil {
             W185("w185"),
             W342("w342"),
             W500("w500"),
-            W780("w780"),
+            W780("w780")
         }
 
         enum class Profile(override val value: String) : Size {
             W45("w45"),
             W185("w185"),
-            H632("h632"),
+            H632("h632")
         }
 
         enum class Still(override val value: String) : Size {
             W92("w92"),
             W185("w185"),
-            W300("w300"),
+            W300("w300")
         }
 
         data object Original : Size {
@@ -70,8 +70,6 @@ object ImageUtil {
         }
     }
 
-    fun buildImageUrl(
-        path: String?,
-        size: Size = Size.Original,
-    ): String = "https://image.tmdb.org/t/p/${size.value}/$path"
+    fun buildImageUrl(path: String?, size: Size = Size.Original): String =
+        "https://image.tmdb.org/t/p/${size.value}/$path"
 }
