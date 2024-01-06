0dc9d776f5aa3b08a0de6ea741486c78956894e5
Jerry Okafor <jerryhanksokafor@gmail.com>
2024-01-06 14:56:05 -0700

    feat : GH-52 Refactor auth module

--- feature/auth/src/test/java/me/jerryokafor/ihenkiri/feature/auth/AuthSessionTest.kt
+++ feature/auth/src/test/java/me/jerryokafor/ihenkiri/feature/auth/AuthSessionTest.kt
@@ -24,97 +24,81 @@
 
 package me.jerryokafor.ihenkiri.feature.auth
 
-import android.os.Build
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.test.espresso.intent.rule.IntentsRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import dagger.hilt.android.testing.BindValue
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.HiltTestApplication
-import me.jerryokafor.uitesthiltmanifest.HiltComponentActivity
-import org.junit.Before
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
-import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
-import org.robolectric.shadows.ShadowLog
-
-@RunWith(AndroidJUnit4::class)
-@Config(
-    application = HiltTestApplication::class,
-    sdk = [Build.VERSION_CODES.O],
-    instrumentedPackages = ["androidx.loader.content"],
-    qualifiers = "xlarge",
-)
-@HiltAndroidTest
-class AuthSessionTest {
-    /**
-     * Manages the components' state and is used to perform injection on your test
-     */
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
-
-    /**
-     * Create a temporary folder used to create a Data Store file. This guarantees that
-     * the file is removed in between each test, preventing a crash.
-     */
-    @BindValue
-    @get:Rule(order = 1)
-    val tmpFolder: TemporaryFolder = TemporaryFolder.builder().assureDeletion().build()
-
-    @get:Rule(order = 2)
-    val intentsRule = IntentsRule()
-
-    /**
-     * Use the primary activity to initialize the app normally.
-     */
-    @get:Rule(order = 3)
-    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
-
-//    private var onCompleteLoginClick = 0
-
-    @Before
-    fun setUp() {
-        ShadowLog.stream = System.out
-
-        hiltRule.inject()
-    }
-
-//    @Test
-//    fun testAuth2() {
-//        val resultData = Intent().apply {
-//            action = Intent.ACTION_VIEW
-//            data = Uri.parse(Constants.AUTH_REDIRECT_URL)
-//        }
+//@RunWith(AndroidJUnit4::class)
+//@Config(
+//    application = HiltTestApplication::class,
+//    sdk = [Build.VERSION_CODES.O],
+//    instrumentedPackages = ["androidx.loader.content"],
+//    qualifiers = "xlarge",
+//)
+//@HiltAndroidTest
+//class AuthSessionTest {
+//    /**
+//     * Manages the components' state and is used to perform injection on your test
+//     */
+//    @get:Rule(order = 0)
+//    val hiltRule = HiltAndroidRule(this)
 //
-// //        val uri = Uri.parse(Constants.TMDB_AUTH_ACCESS_URL).buildUpon()
-// //            .appendQueryParameter(
-// //                AuthParam.REQUEST_TOKEN,
-// //                createRequestTokenSuccessResponse().requestToken,
-// //            )
-// //            .appendQueryParameter(AuthParam.REDIRECT_TO, Constants.AUTH_REDIRECT_URL)
-// //            .build()
+//    /**
+//     * Create a temporary folder used to create a Data Store file. This guarantees that
+//     * the file is removed in between each test, preventing a crash.
+//     */
+//    @BindValue
+//    @get:Rule(order = 1)
+//    val tmpFolder: TemporaryFolder = TemporaryFolder.builder().assureDeletion().build()
 //
-//        val expected = allOf(hasAction(Intent.ACTION_VIEW))
-//        Intents.intending(expected).respondWith(
-//            Instrumentation.ActivityResult(Activity.RESULT_OK, resultData),
-//        )
+//    @get:Rule(order = 2)
+//    val intentsRule = IntentsRule()
 //
-//        composeTestRule.apply {
-//            setContent {
-//                AuthScreen(
-//                    onCompleteLogin = { onCompleteLoginClick++ },
-//                    onShowSnackbar = { _, _ -> true },
-//                )
-//            }
-//            onNodeWithText("Sign In").performClick()
-// //            onNodeWithText("Continue as Guest").performClick()
-// //            onRoot(true).printToLog("Testing")
-//            waitForIdle()
-//        }
+//    /**
+//     * Use the primary activity to initialize the app normally.
+//     */
+//    @get:Rule(order = 3)
+//    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
 //
-//        Intents.intended(expected)
-//        assertThat(onCompleteLoginClick).isEqualTo(1)
+////    private var onCompleteLoginClick = 0
+//
+//    @Before
+//    fun setUp() {
+//        ShadowLog.stream = System.out
+//
+//        hiltRule.inject()
 //    }
-}
+//
+////    @Test
+////    fun testAuth2() {
+////        val resultData = Intent().apply {
+////            action = Intent.ACTION_VIEW
+////            data = Uri.parse(Constants.AUTH_REDIRECT_URL)
+////        }
+////
+//// //        val uri = Uri.parse(Constants.TMDB_AUTH_ACCESS_URL).buildUpon()
+//// //            .appendQueryParameter(
+//// //                AuthParam.REQUEST_TOKEN,
+//// //                createRequestTokenSuccessResponse().requestToken,
+//// //            )
+//// //            .appendQueryParameter(AuthParam.REDIRECT_TO, Constants.AUTH_REDIRECT_URL)
+//// //            .build()
+////
+////        val expected = allOf(hasAction(Intent.ACTION_VIEW))
+////        Intents.intending(expected).respondWith(
+////            Instrumentation.ActivityResult(Activity.RESULT_OK, resultData),
+////        )
+////
+////        composeTestRule.apply {
+////            setContent {
+////                AuthScreen(
+////                    onCompleteLogin = { onCompleteLoginClick++ },
+////                    onShowSnackbar = { _, _ -> true },
+////                )
+////            }
+////            onNodeWithText("Sign In").performClick()
+//// //            onNodeWithText("Continue as Guest").performClick()
+//// //            onRoot(true).printToLog("Testing")
+////            waitForIdle()
+////        }
+////
+////        Intents.intended(expected)
+////        assertThat(onCompleteLoginClick).isEqualTo(1)
+////    }
+//}
